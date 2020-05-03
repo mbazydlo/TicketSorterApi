f5bee70d83141e044b32a80bcc0d4500e7a9c8e2
Marcin Bazydło <marcin.p.bazydlo@gmail.com>
2020-05-04 00:02:04 +0200

    v0.6 Add two additional tests
    Test test_correct_data checks API with correct JSON
    Test test_empty_list_of_tickets checks API with empty JSON

--- tests/test_response.py
+++ tests/test_response.py
@@ -51,3 +51,59 @@ class ResponseTest(unittest.TestCase):
         response = self.app.post('/tickets', headers={"Content-Type": "application/json"}, data=tickets)
 
         self.assertEqual('Wrong tickets', response.json['response'])
+
+    # Case of proper list - correct data.
+    def test_correct_data(self):
+        tickets = json.dumps([
+                            { 
+                             "id" : 1,
+                                "start" : "Warszawa",
+                                "finish": "Krakow",
+                                "transport_number" : "123",
+                                "mean_of_transport" : "train",
+                                "seat_assigment" : "A34"
+                            },
+                            {   
+                                "id" : 2,
+                                "start" : "Gdansk",
+                                "finish": "Poznan",
+                                "transport_number" : "T2464",
+                                "mean_of_transport" : "flight",
+                                "seat_assigment" : "13C",
+                                "baggage" : "345",
+                                "gate" : "Z5"
+                            },
+                            {   
+                                "id" : 3,
+                                "start" : "Krakow",
+                                "finish": "Gdansk",
+                                "transport_number" : "T24",
+                                "mean_of_transport" : "bus"
+                            },
+                            {
+                                "id" : 4,
+                                "start" : "Poznan",
+                                "finish": "Berlin",
+                                "transport_number" : "Z5730",
+                                "mean_of_transport" : "flight",
+                                "seat_assigment" : "25E",
+                                "gate" : "Z5"
+                            }])
+
+        expected = [
+                    "1. Take train 123 from Warszawa to Krakow. Sit in A34",
+                    "2. Take bus T24 from Krakow to Gdansk. No seat assigned",
+                    "3. From Gdansk, take flight T2464 to Poznan. Gate Z5. Seat 13C. Baggage drop at ticket counter 345.",
+                    "4. From Poznan, take flight Z5730 to Berlin. Gate Z5. Seat 25E. Baggage will we automatically transferred from your last leg.",
+                    "5. You have arrived at your final destination."
+                    ]
+
+        response = self.app.post('/tickets', headers={"Content-Type": "application/json"}, data=tickets)
+        self.assertEqual(expected, response.json)
+
+    # Case of empty list - no tickets.
+    def test_empty_list_of_tickets(self):
+        tickets = json.dumps([])
+        response = self.app.post('/tickets', headers={"Content-Type": "application/json"}, data=tickets)
+
+        self.assertEqual('Wrong tickets', response.json['response'])
